b54b4474de731e5bb381a71f71322dbc0f58c33d
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-01-17 09:00:12 +0200

    Fix updating package.json and fix errors

--- client/src/back.js
+++ client/src/back.js
@@ -1,69 +1,65 @@
 export const Users = [
-{
-    name: 'Alex',
-    _id: '12341asd',
+  {
+    name: "Alex",
+    _id: "12341asd",
     last_name: `lol`,
-    password : '1243',
-    email : "cas@cas.a",
-    phone_numer : 1234124,
-    isAdmin : false,
+    password: "1243",
+    email: "cas@cas.a",
+    phone_numer: 1234124,
+    isAdmin: false,
     pets: [
-        {
-            _id : '12',
-
-        },
+      {
+        _id: "12",
+      },
     ],
-},
-{
-    name: 'Alex',
-    _id: '12341asd',
+  },
+  {
+    name: "Alex",
+    _id: "12341asd",
     last_name: `lol`,
-    password : '1243',
-    email : "cas@cas.a",
-    phone_numer : 1234124,
-    isAdmin : false,
+    password: "1243",
+    email: "cas@cas.a",
+    phone_numer: 1234124,
+    isAdmin: false,
     pets: [
-        {
-            _id : '13',
-
-        },
+      {
+        _id: "13",
+      },
     ],
-},
-]
+  },
+];
 
-export const Pets =[
-     {
- Type: 'dog', 
- Name: `sss`,
- Adoption_Status: {
-    isAdoption:  true, 
-    isReturned: false,
- }, 
- Picture: `img`, 
- Height: 150, 
- Weight: 44, 
- Color: `red`, 
- Bio: `asdasd adsdas`, 
- Hypoallergenic: false, 
- dietary_restrictions: `a` , 
- breed_of_animal: `Poodle, Siamese`,
-
-},
-     {
- Type: 'cat', 
- Name: `ss`,
- Adoption_Status: {
-    isAdoption:  false, 
-    isReturned: true,
- },
- Picture: `img`, 
- Height: 150, 
- Weight: 44, 
- Color: `red`, 
- Bio: `asdasd adsdas`, 
- Hypoallergenic: false, 
- dietary_restrictions: `a` , 
- breed_of_animal: `Poodle, Siamese`,
-
-},
-]
+export const Pets = [
+  {
+    Type: "dog",
+    Name: `sss`,
+    Adoption_Status: {
+      isAdoption: true,
+      isReturned: false,
+    },
+    Picture: `img`,
+    Height: 150,
+    Weight: 44,
+    Color: `red`,
+    Bio: `asdasd adsdas`,
+    Hypoallergenic: false,
+    dietary_restrictions: `a`,
+    breed_of_animal: `Poodle, Siamese`,
+  },
+  {
+    Type: "cat",
+    Name: `ss`,
+    Adoption_Status: {
+      isAdoption: false,
+      isReturned: true,
+    },
+    Picture: `img`,
+    Height: 150,
+    Weight: 44,
+    Color: `red`,
+    Bio: `asdasd adsdas`,
+    Hypoallergenic: false,
+    dietary_restrictions: `a`,
+    breed_of_animal: `Poodle, Siamese`,
+  },
+];
